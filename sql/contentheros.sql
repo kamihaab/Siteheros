b87da08b5c82725b38ab578b1845c804cb146670
paulixe <pchaumont@ensc.fr>
2022-04-27 14:12:42 +0200

    bdd histoire_branche_id

--- sql/contentheros.sql
+++ sql/contentheros.sql
@@ -10,10 +10,18 @@ ALTER TABLE histoire auto_increment=1;
 DELETE FROM  histoireEnCours;
 DELETE FROM brancheabranche;
 
-insert into histoire(histoire_titre,histoire_image,histoire_resume) values
-("OUIIIIIIIIII","escargot","WHEEEEEEEEEEEEEEEEEEEE, cet escargot refait votre journée"),
-("LEEEEE RATATAT","ratatouille","Vous incarnez un rat, WOWOWOWOOWOWOWOWOWO!!!!"),
-("UNEE patatae","patate","Epreuve : éplucher la patate"),
-("Fait Chaud","desert","LEEE SOLEIELLLLL CA BRULELLE LEL LE E ELLEL"),
-("L'eau ca mouille","mer","Epreuve: mouiller quelqu'un d'autre puis ne pas pleurer quand on nous renvoie de l'eau"),
-("ZEEENITSUU L'ECLAIR","Zenitsu","Rejoignez une hisoire palpitante dans laquelle vous incarnez Zenitsu.  Vous devez fuir tous démons se présentant à vous. Cependant attention à ne pas oublier de crier TANJIROROROROROROROROO à chaque instant si vous voulez gagner. De plus regarder Nezuko vous fait gagner la partie d’un coup !");
+insert into histoire(histoire_titre,histoire_image,histoire_resume,histoire_branche_id) values
+("OUIIIIIIIIII","escargot","WHEEEEEEEEEEEEEEEEEEEE, cet escargot refait votre journée",1),
+("LEEEEE RATATAT","ratatouille","Vous incarnez un rat, WOWOWOWOOWOWOWOWOWO!!!!",2),
+("UNEE patatae","patate","Epreuve : éplucher la patate",3),
+("Fait Chaud","desert","LEEE SOLEIELLLLL CA BRULELLE LEL LE E ELLEL",4),
+("L'eau ca mouille","mer","Epreuve: mouiller quelqu'un d'autre puis ne pas pleurer quand on nous renvoie de l'eau",5),
+("ZEEENITSUU L'ECLAIR","Zenitsu","Rejoignez une hisoire palpitante dans laquelle vous incarnez Zenitsu.  Vous devez fuir tous démons se présentant à vous. Cependant attention à ne pas oublier de crier TANJIROROROROROROROROO à chaque instant si vous voulez gagner. De plus regarder Nezuko vous fait gagner la partie d’un coup !",6);
+
+insert into branche(branche_titre,branche_paragraphe,branche_image,branche_histoire_id) values
+("branche1","escargot","l'escargot de la mortttttt",1),
+("LEEEEE RATATAT","ratatouille","Vous incarnez un rat, WOWOWOWOOWOWOWOWOWO!!!!",2),
+("UNEE patatae","patate","Epreuve : éplucher la patate",3),
+("Fait Chaud","desert","LEEE SOLEIELLLLL CA BRULELLE LEL LE E ELLEL",4),
+("L'eau ca mouille","mer","Epreuve: mouiller quelqu'un d'autre puis ne pas pleurer quand on nous renvoie de l'eau",5),
+("ZEEENITSUU L'ECLAIR","Zenitsu","Rejoignez une hisoire palpitante dans laquelle vous incarnez Zenitsu.  Vous devez fuir tous démons se présentant à vous. Cependant attention à ne pas oublier de crier TANJIROROROROROROROROO à chaque instant si vous voulez gagner. De plus regarder Nezuko vous fait gagner la partie d’un coup !",6);
--- sql/structureheros.sql
+++ sql/structureheros.sql
@@ -1,8 +1,9 @@
-drop table if exists user;
-drop table if exists branche;
-drop table if exists histoire;
 drop table if exists histoireEnCours;
 drop table if exists brancheabranche;
+drop table if exists branche;
+drop table if exists histoire;
+drop table if exists user;
+
 
 create table user(
     usr_id integer not null primary key auto_increment,
@@ -10,30 +11,34 @@ create table user(
     usr_password varchar(88) not null
 ) engine=innodb character set utf8 collate utf8_unicode_ci;
 
-create table branche(
-    branche_id integer not null primary key auto_increment,
-    branche_titre varchar(20),
-    branche_paragraphe varchar(500),
-    branche_image varchar(150)
-) engine=innodb character set utf8 collate utf8_unicode_ci;
-
 create table histoire(
     histoire_id integer not null primary key auto_increment,
     histoire_titre varchar(50) not null,
     histoire_image varchar(150),
-    histoire_resume varchar (500)
+    histoire_resume varchar (500),
+    histoire_branche_id integer
+
+) engine=innodb character set utf8 collate utf8_unicode_ci;
+
+create table branche(
+    branche_id integer not null primary key auto_increment,
+    branche_titre varchar(20),
+    branche_paragraphe varchar(500),
+    branche_image varchar(150),
+    branche_histoire_id integer,
+    FOREIGN KEY (branche_histoire_id) references histoire(histoire_id) ON DELETE CASCADE
 ) engine=innodb character set utf8 collate utf8_unicode_ci;
 
 create table histoireEnCours (
     histoireEnCours_usr_id integer,
     histoireEnCours_branche_id integer,
-    FOREIGN KEY (histoireEnCours_usr_id) references user(usr_id),
-    FOREIGN KEY (histoireEnCours_branche_id) references branche(branche_id)
+    FOREIGN KEY (histoireEnCours_usr_id) references user(usr_id) ON DELETE CASCADE,
+    FOREIGN KEY (histoireEnCours_branche_id) references branche(branche_id)ON DELETE CASCADE
 ) engine=innodb character set utf8 collate utf8_unicode_ci;
 
 create table brancheabranche (
     brancheabranche_brancheprecedente_id integer,
     brancheabranche_branchesuivante_id integer,
-    FOREIGN KEY (brancheabranche_brancheprecedente_id) references branche(branche_id),
-    FOREIGN KEY (brancheabranche_branchesuivante_id) references branche(branche_id)
+    FOREIGN KEY (brancheabranche_brancheprecedente_id) references branche(branche_id) ON DELETE CASCADE,
+    FOREIGN KEY (brancheabranche_branchesuivante_id) references branche(branche_id) ON DELETE CASCADE
 ) engine=innodb character set utf8 collate utf8_unicode_ci;
