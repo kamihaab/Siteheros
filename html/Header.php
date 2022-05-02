fdf732d9d2d0cfe6ab83e306daae8de70013a8af
kamihaab <chaaby@ensc.fr>
2022-05-02 18:16:07 +0200

    Bonjour, login

--- html/Header.php
+++ html/Header.php
@@ -1,5 +1,15 @@
+<?php require_once "connect.php"; 
+session_start();
+?>
+
 <header>
         <a href="index.php" ><h1> Le Site dont tu es le Héros
         </h1> </a>
+        <?php if (isUserConnected()){ ?>
+            Bienvenue, <?= $_SESSION['login'] ?>
+            <a class="bouton connexion" href="logout.php">Déconnexion</a>
+        <?php } else { ?>
         <a class="bouton connexion" href="login.php">Connexion</a>
+        <?php } ?>
+
     </header>
