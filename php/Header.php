488d28377f6e37f385c2f2f4b82185adb81b61a0
paulixe <pchaumont@ensc.fr>
2022-05-02 19:51:31 +0200

    dz

--- php/Header.php
+++ php/Header.php
@@ -1,6 +1,4 @@
 
-
-
 <header>
         <a href="index.php" ><h1> Le Site dont tu es le Héros
         </h1> </a>
@@ -8,7 +6,7 @@
 //Don't forget session_start in other pages 
     if (isset( $_SESSION['loggedin'])&& $_SESSION['loggedin']==true)
     {
-        ?>Bienvenue, <?= $_SESSION['login']?>
+        ?>
         <a class="bouton connexion" href="fonctions/deconnexion.php">Se déconnecter</a>
         <?php
     }
