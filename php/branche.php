4531ba28c2796bbe7d08f8da1da15c11cd79550c
kamihaab <chaaby@ensc.fr>
2022-05-05 23:59:05 +0200

    Modifs bdd

--- php/branche.php
+++ php/branche.php
@@ -25,6 +25,50 @@ session_start();
             $nomImage = $images[0];
             $titre = $ligne['branche_titre'];
             $paragraphe=$ligne['branche_paragraphe'];
+
+            ////////////////////////////////////////////////
+            $histoire=$ligne['branche_histoire_id']; //on recupère l'histoire associée à la branche
+            $requeteVieInitiale="SELECT histoire_vie FROM histoire WHERE histoire_branche_id ='$histoire'"; //on prend la vie initiale de l'histoire
+            $res=$bdd->query($requeteVieInitiale);
+            $ligne=$res->fetch();
+            $vieinitiale = $ligne['histoire_vie'];
+            $vie;
+            //On regarde si la table histoire en cours est vide 
+            $reqCheck ="SELECT histoireEnCours_vie FROM histoireEnCours"; //Where avec le user_id??
+            if(empty($reqCheck)){
+                $vie=$vieinitiale;
+                $requete = $bdd->prepare("INSERT INTO histoireEnCours (`histoireEnCours_vie`) VALUES(?)"); 
+                $requete->execute(array($vie));
+
+            }
+            else{
+                $requeteVie="SELECT histoireEnCours_vie FROM histoireEnCours WHERE histoireEnCours_branche_id ='$_GET[id]'";
+                $res=$bdd->query($requeteVie);
+                $ligne=$res->fetch();
+                $vie=$ligne['histoireEnCours_vie']- $ligne['branche_vie'];
+                //On va maintenant enregistrer la variable vie dans la table histoire en cours associée au user_id
+            //$requeteUser="SELECT * FROM branche WHERE branche_id='$_GET[id]'";
+            //$_SESSION['login']
+                $sql = "UPDATE histoireEnCours
+                SET histoireEnCours_vie='$vie'
+                WHERE histoireEnCours_branche_id ='$_GET[id]'";
+                $bdd->query($sql);
+
+
+
+            }
+            /*Ancienne technique
+            if ($ligne['branche_id']!=='2'){
+                $vie=$ligne['histoireEnCours_vie'] - $ligne['branche_vie'];
+                //vieencours dans histoire en cours 
+                //vie initiale dans histoire pour laisser le mec choisir
+
+            }
+            if ($ligne['branche_id']==='2'){
+                $vie=$vieinitiale - $ligne['branche_vie'];
+            }
+            */
+          //////////////////////////////////////////////////////////////////////////////////////
             ?>
 
             <img class="FondBranche" src="<?= $nomImage ?>" alt="image de <?= $nomImage ?>">
@@ -32,6 +76,12 @@ session_start();
             <br><br><br><br><br>
             <p><?=$paragraphe?></p>
             <br><br><br>
+            <!-- On ajoute la vie -->
+            <p>
+            <img class="pv" src="../image/vie.png" alt="image coeur">
+            <?=$vie?></p>
+            <br><br><br>
+            <!---->
             <?php
         $sql = "SELECT * FROM brancheabranche WHERE brancheabranche_brancheactuelle_id ='$_GET[id]'"; //ou =branche_id? 
         $res = $bdd->query($sql);
--- sql/contentheros.sql
+++ sql/contentheros.sql
@@ -10,25 +10,25 @@ ALTER TABLE histoire auto_increment=1;
 DELETE FROM  histoireEnCours;
 DELETE FROM brancheabranche;
 
-insert into histoire(histoire_titre,histoire_image,histoire_resume,histoire_branche_id) values
-("OUIIIIIIIIII","escargot","WHEEEEEEEEEEEEEEEEEEEE, cet escargot refait votre journée",1),
-("LEEEEE RATATAT","ratatouille","Vous incarnez un rat, WOWOWOWOOWOWOWOWOWO!!!!",2),
-("UNEE patatae","patate","Epreuve : éplucher la patate",3),
-("Fait Chaud","desert","LEEE SOLEIELLLLL CA BRULELLE LEL LE E ELLEL",4),
-("L'eau ca mouille","mer","Epreuve: mouiller quelqu'un d'autre puis ne pas pleurer quand on nous renvoie de l'eau",5),
-("ZEEENITSUU L'ECLAIR","Zenitsu","Rejoignez une hisoire palpitante dans laquelle vous incarnez Zenitsu.  Vous devez fuir tous démons se présentant à vous. Cependant attention à ne pas oublier de crier TANJIROROROROROROROROO à chaque instant si vous voulez gagner. De plus regarder Nezuko vous fait gagner la partie d’un coup !",6);
+insert into histoire(histoire_titre,histoire_image,histoire_resume,histoire_branche_id,histoire_vie) values
+("OUIIIIIIIIII","escargot","WHEEEEEEEEEEEEEEEEEEEE, cet escargot refait votre journée",1,10),
+("LEEEEE RATATAT","ratatouille","Vous incarnez un rat, WOWOWOWOOWOWOWOWOWO!!!!",2,10),
+("UNEE patatae","patate","Epreuve : éplucher la patate",3,10),
+("Fait Chaud","desert","LEEE SOLEIELLLLL CA BRULELLE LEL LE E ELLEL",4,10),
+("L'eau ca mouille","mer","Epreuve: mouiller quelqu'un d'autre puis ne pas pleurer quand on nous renvoie de l'eau",5,10),
+("ZEEENITSUU L'ECLAIR","Zenitsu","Rejoignez une hisoire palpitante dans laquelle vous incarnez Zenitsu.  Vous devez fuir tous démons se présentant à vous. Cependant attention à ne pas oublier de crier TANJIROROROROROROROROO à chaque instant si vous voulez gagner. De plus regarder Nezuko vous fait gagner la partie d’un coup !",6,10);
 
-insert into branche(branche_titre,branche_paragraphe,branche_image,branche_histoire_id) values
-("branche1","Vous vous baladez et soudainnnnn vous rencontre cet escargot, qui fait weweeeewewewewewewewewe","escargot",1),
-("LEEEEE RATATAT","Il etait une fois un petit rat zdedzedzed","ratatouille",2),
-("UNEE patatae","Il ne reste que vous,la patate et l'éplucheur de patate","patate",3),
-("Fait Chaud","Vous êtes parti pour un run et vous vous êtes perdu dans vos pensées","desert",4),
-("L'eau ca mouille","JE VEUX ALLLLER A LA PAGGE ALED, ARRETER DE DONNER DES PROJETS","mer",5),
-("ZEEENITSUU L'ECLAIR","Vous rentrez chez vous et soudain vous voyez votre famiile morte par terre","Zenitsu",6);
+insert into branche(branche_titre,branche_paragraphe,branche_image,branche_histoire_id,branche_vie) values
+("branche1","Vous vous baladez et soudainnnnn vous rencontre cet escargot, qui fait weweeeewewewewewewewewe","escargot",1,0),
+("LEEEEE RATATAT","Il etait une fois un petit rat zdedzedzed","ratatouille",2,0),
+("UNEE patatae","Il ne reste que vous,la patate et l'éplucheur de patate","patate",3,0),
+("Fait Chaud","Vous êtes parti pour un run et vous vous êtes perdu dans vos pensées","desert",4,0),
+("L'eau ca mouille","JE VEUX ALLLLER A LA PAGGE ALED, ARRETER DE DONNER DES PROJETS","mer",5,0),
+("ZEEENITSUU L'ECLAIR","Vous rentrez chez vous et soudain vous voyez votre famiile morte par terre","Zenitsu",6,0);
 
-insert into branche(branche_titre,branche_paragraphe,branche_image,branche_histoire_id) values
-("branche2.a","Vous courrez","escargot",1),
-("branche2.b","vous sautez","escargot",1),
+insert into branche(branche_titre,branche_paragraphe,branche_image,branche_histoire_id,branche_vie) values
+("branche2.a","Vous courrez","escargot",1,2),
+("branche2.b","vous sautez","escargot",1,3),
 ("branche2.c","vous crier ayayyaayyaayayayayya!!!","escargot",1);
 
 insert into brancheabranche(brancheabranche_nombouton,brancheabranche_brancheactuelle_id,brancheabranche_branchesuivante_id) values
