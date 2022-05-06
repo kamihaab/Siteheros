b1e419668a9c46f0246b20508558f4a34b489a92
kamihaab <chaaby@ensc.fr>
2022-05-06 11:18:13 +0200

    cara

--- php/fonctions/initialisationHistoire.php
+++ php/fonctions/initialisationHistoire.php
@@ -1,18 +1,25 @@
 <?php
 session_start();
 require_once("connect.php");
-        if (isset($_GET['idhistoire'])) { 
+
+        if (isset($_SESSION['iduser'])&&isset($_GET['idhistoire'])&&isset($_GET['idbranche'])) { 
             $idbranche=$_GET['idbranche'];
             $idhistoire=$_GET['idhistoire'];
-            $requeteVieInitiale="SELECT histoire_vie FROM histoire WHERE histoire_id=$idhistoire"; //on prend la vie initiale de l'histoire
+            $requeteVieInitiale="SELECT histoire_vie FROM histoire WHERE histoire_id='$idhistoire'"; //on prend la vie initiale de l'histoire
             $resVie=$bdd->query($requeteVieInitiale);
             $ligneVie=$resVie->fetch();
-            $vieinitiale = $ligne['histoire_vie'];
-            $requete = $bdd->prepare("INSERT INTO histoireEnCours (`histoireEnCours_vie`,`histoireEnCours_branche_id`) VALUES(?,?)"); 
-            $requete->execute(array($vieinitiale,$idbranche));
+            print_r($ligneVie);
+            $idUser=$_SESSION['iduser'];
+            $vieinitiale = $ligneVie['histoire_vie'];
+            $requete = $bdd->prepare("INSERT INTO histoireEnCours (`histoireEnCours_usr_id`,`histoireEnCours_vie`,`histoireEnCours_branche_id`) VALUES(?,?,?)"); 
+            $requete->execute(array($idUser,$vieinitiale,$idbranche));
             header('location: ../branche.php?id='.$idbranche);
             exit;
 
+        }
+        else{
+            header('location: ../index.php');
+            exit;
         }
     
 ?>
